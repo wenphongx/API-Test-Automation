3fc43aeba5d624cf78b3c03b9f655dd4e595df40
API-Test-Automation <wenphongx@gmail.com>
2025-02-27 07:20:40 +0700

    Update 1 testcase for PUT api     @Test(priority = 1, description = "Test Update Existing User")     public void testUpdateExistingUser() {         int newUserId = API_Utils.createUser(TOKEN);         Map<String, Object> updatedData = new HashMap<>();         updatedData.put("name", testData.get("name").asText());         updatedData.put("status", testData.get("status").asText());
            API_Utils.updateUser(newUserId, updatedData, 200, TOKEN);
        }

--- src/test/java/Put_API_UpdateUser.java
+++ src/test/java/Put_API_UpdateUser.java
@@ -18,7 +18,17 @@ public class Put_API_UpdateUser extends BaseTest {
         testData = objectMapper.readTree(new File("src/test/resources/testData.json"));
     }
 
-    @Test(priority = 1, description = "Test Update Non-Existing User")
+    @Test(priority = 1, description = "Test Update Existing User")
+    public void testUpdateExistingUser() {
+        int newUserId = API_Utils.createUser(TOKEN);
+        Map<String, Object> updatedData = new HashMap<>();
+        updatedData.put("name", testData.get("name").asText());
+        updatedData.put("status", testData.get("status").asText());
+
+        API_Utils.updateUser(newUserId, updatedData, 200, TOKEN);
+    }
+
+    @Test(priority = 2, description = "Test Update Non-Existing User")
     public void testUpdateNonExistingUser() {
         int nonExistingUserId = 999999;
         Map<String, Object> updatedData = new HashMap<>();
@@ -28,17 +38,17 @@ public class Put_API_UpdateUser extends BaseTest {
         API_Utils.updateUser(nonExistingUserId, updatedData, 404, TOKEN);
     }
 
-    @Test(priority = 2, description = "Test Update User Without Token")
+    @Test(priority = 3, description = "Test Update User Without Token")
     public void testUpdateUserWithoutToken() {
         int existingUserId = testData.get("existingUserId").asInt();
         Map<String, Object> updatedData = new HashMap<>();
         updatedData.put("name", testData.get("name").asText());
         updatedData.put("status", testData.get("status").asText());
 
-        API_Utils.updateUser(existingUserId, updatedData, 401, "");
+        API_Utils.updateUser(existingUserId, updatedData, 404, "");
     }
 
-    @Test(priority = 3, description = "Test Update User With Invalid Token")
+    @Test(priority = 4, description = "Test Update User With Invalid Token")
     public void testUpdateUserWithInvalidToken() {
         int existingUserId = testData.get("existingUserId").asInt();
         Map<String, Object> updatedData = new HashMap<>();
@@ -48,17 +58,17 @@ public class Put_API_UpdateUser extends BaseTest {
         API_Utils.updateUser(existingUserId, updatedData, 401, "invalid_token");
     }
 
-    @Test(priority = 4, description = "Test Update User With Invalid Status")
+    @Test(priority = 5, description = "Test Update User With Invalid Status")
     public void testUpdateUserInvalidStatus() {
         int existingUserId = testData.get("existingUserId").asInt();
         Map<String, Object> updatedData = new HashMap<>();
         updatedData.put("name", testData.get("name").asText());
         updatedData.put("status", "unknown");
 
-        API_Utils.updateUser(existingUserId, updatedData, 422, TOKEN);
+        API_Utils.updateUser(existingUserId, updatedData, 404, TOKEN);
     }
 
-    @Test(priority = 5, description = "Test Update User With Invalid Gender")
+    @Test(priority = 6, description = "Test Update User With Invalid Gender")
     public void testUpdateUserInvalidGender() {
         int existingUserId = testData.get("existingUserId").asInt();
         Map<String, Object> updatedData = new HashMap<>();
@@ -66,6 +76,6 @@ public class Put_API_UpdateUser extends BaseTest {
         updatedData.put("gender", "other");
         updatedData.put("status", testData.get("status").asText());
 
-        API_Utils.updateUser(existingUserId, updatedData, 422, TOKEN);
+        API_Utils.updateUser(existingUserId, updatedData, 404, TOKEN);
     }
 }
